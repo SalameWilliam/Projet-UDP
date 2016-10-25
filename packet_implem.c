2b6912b8a5318c27bb311e742746a50bdfa7acec
Kiwii <william.salame@student.uclouvain.be>
2016-10-25 16:46:28 +0200

    Decode Encode

--- packet_implem.c
+++ packet_implem.c
@@ -3,17 +3,11 @@
 #include <memory.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <zlib.h>
 #include "packet_interface.h"
 
-
-void main(int argc, char** argv){
-    pkt_t* oui = pkt_new();
-    pkt_set_window(oui,3);
-    size_t size1 = sizeof(pkt_get_window(oui));
-    pkt_set_seqnum(oui,3);
-    size_t size2 = sizeof(pkt_get_type(oui));
-    printf("oui");
-}
+#define HEADER_BYTES 4
+#define SIZE_CRC 4
 
 struct __attribute__((__packed__)) pkt {
      ptypes_t type : 3;
@@ -32,22 +26,86 @@ pkt_t* pkt_new()
 
 void pkt_del(pkt_t *pkt)
 {
-    free(pkt->payload);
-    pkt->payload = NULL;
     free(pkt);
 }
 
 pkt_status_code pkt_decode(const char *data, const size_t len, pkt_t *pkt)
 {
     //TODO : Need to encode stuff en shift bits to make them round that's all. Do not forget to put window and type together to make an eight bit (look at git for more info)
-    read(0,(void*)data, sizeof(data));
+
+    //prise de type
+    ptypes_t type = (ptypes_t) (data[0] >> 5);
+
+    if(type!=PTYPE_DATA && type!= PTYPE_ACK){
+        return E_TYPE;
+    }
+
+    //prise de Window
+    uint8_t wi1=(uint8_t) (data[0] << 3);
+    uint8_t window = (wi1 >> 3);
+
+    uint8_t seqnum =(uint8_t) data[1];
+
+    uint16_t length =(uint16_t) (data[2] << 8|data[3]);
+
+    size_t size_payload = len - HEADER_BYTES - SIZE_CRC;
+
+    char *buffer = (char*)malloc(size_payload);
+
+    memcpy((void *)buffer,(void *)&data[4],size_payload);
+
+    //uint32_t current_crc = (uint32_t) crc32(0, (const void*)data, HEADER_BYTES+length);
+
+    uint32_t old_crc;
+
+    memcpy((void *)&old_crc,(void *)&data[HEADER_BYTES + size_payload],SIZE_CRC);
+
+    //if(old_crc != current_crc)
+        //return E_CRC;
+
+    pkt_set_type(pkt,type);
+    pkt_set_seqnum(pkt,seqnum);
+    pkt_set_window(pkt,window);
+    pkt_set_length(pkt,length);
+    pkt_set_payload(pkt,buffer,(uint16_t)size_payload);
+    pkt_set_crc(pkt,old_crc);
+
     return PKT_OK;
 }
 
 pkt_status_code pkt_encode(const pkt_t* pkt, char *buf, size_t *len)
 {
     //TODO : Decode with taking each byte from the buf en shifting them if they are not round
-    write(buf,(void*)pkt->type,len);
+
+    if (*len < sizeof(pkt)){
+        return E_NOMEM;
+    }
+
+    ptypes_t type = pkt_get_type(pkt);
+    uint8_t window = pkt_get_window(pkt);
+
+    buf[0] = (type<<5)|window;
+
+    uint8_t seqnum = pkt_get_seqnum(pkt);
+
+    buf[1] = seqnum;
+
+    uint16_t length = pkt_get_length(pkt);
+
+    memcpy((void *)&buf[2],(void *)&length, sizeof(length));
+
+    const char* data = pkt_get_payload(pkt);
+
+    memcpy((void *)&buf[4],data,length);
+
+    //uint32_t crc = (uint32_t) crc32(0, (const void*)buf, HEADER_BYTES+length);
+
+    uint32_t crc = pkt_get_crc(pkt);
+
+    memcpy((void *)&buf[HEADER_BYTES+length],(void *) &crc, SIZE_CRC);
+
+    *len = HEADER_BYTES + length + SIZE_CRC;
+
     return PKT_OK;
 }
 
@@ -127,6 +185,9 @@ pkt_status_code pkt_set_payload(pkt_t *pkt,
                                 const char *data,
                                 const uint16_t length)
 {
+    pkt->payload = (char *)malloc(length);
     memcpy((void*)pkt->payload,(void*)data,length);
+    pkt->length = length;
+
     return PKT_OK;
 }
